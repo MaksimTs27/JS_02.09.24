9f479f409688726e77d88559bfc5d424f6bfd20e
MaksimTs27 <tsedilin.maksim@mail.ru>
2024-09-02 17:41:50 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+//Variabels
+//task 1
+
+const greeting = 'Hello World!';
+console.log(greeting);
+
+//task2
+
+const a = 5;
+const b = 10;
+const sum = a + b;
+console.log(sum);
+
+//task3
+
+let name = 'Alice';
+name = 'Bob';
+console.log(name);
+
+//Data types
+//task1
+
+const hello = 'Hello';
+const age = 42;
+const boolean = true;
+
+console.log(typeof hello, typeof age, typeof boolean);
+
+//task2
+
+let age2 = 25;
+let newAge2 = String(age2);
+console.log(newAge2);
+
+//task3
+
+let firstName = 'John';
+let lastName = 'Doe';
+const fullName = firstName + lastName;
+console.log(fullName);
+
+//Convertion
+//task1
+
+let strNumber = '100';
+let newstrNumber = Number(strNumber);
+console.log(newstrNumber);
+
+//task2
+
+let score = 95;
+let conversScore = String(score);
+console.log(conversScore);
+
+//task3
+
+let isLoggedIn = true;
+let conversIsLoggedIn = String(isLoggedIn);
+console.log(conversIsLoggedIn);
